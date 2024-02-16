9f70bfd77d5d40964e3cd36d951bf0f09dc6237d
Shane Pearson <bubbafoley@gmail.com>
2024-02-16 22:22:31 +0000

    update packages and add routes

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.use(cors())
-app.use(express.static('public'))
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+async function connect() {
+  await mongoose.connect(process.env.MONGO_URI);
+}
+connect()
+  .then(() => console.log('connected to MongoDB'))
+  .catch(err => console.log(err));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+app.post('/api/users', (req, res) => {
+  return res.json([]);
+});
 
+app.get('/api/users', (req, res) => {});
 
+app.post('/api/users/:_id/exercises', (req, res) => {});
+
+app.get('/api/users/:_id/logs', (req, res) => {});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
